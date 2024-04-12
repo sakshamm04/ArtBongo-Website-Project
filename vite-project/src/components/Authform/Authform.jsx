22ae0554847721c76b90afe232723f513ea65851
Saksham <pathaksaksham566@gmail.com>
2024-04-13 00:54:08 +0530

    Updated GoogleAuth and AuthForm

--- vite-project/src/components/Authform/Authform.jsx
+++ vite-project/src/components/Authform/Authform.jsx
@@ -19,7 +19,7 @@ const Authform = () => {
                     <Text mx={1} color={"white"}>OR</Text>
                     <Box flex={2} h={"1px"} bg={"gray.400"}/>
                 </Flex>
-                <Googleauth />
+                <Googleauth prefix = {islogin ? "Log in" : "Sign Up"}/>
             </VStack>
         </Box>
         <Box className='buxa2' border={"1px solid gray"} borderRadius={10} padding={4}>
@@ -37,48 +37,3 @@ const Authform = () => {
 }
 
 export default Authform
-
-// import { Box, Flex, Image, Text, VStack } from '@chakra-ui/react'
-// import React, { useState } from 'react'
-// import './Authform.css'
-// import Login from './Login'
-// import Signup from './Signup'
-// import Googleauth from './Googleauth'
-
-// const Authform = () => {
-//     const [islogin, setlogin] = useState(true)
-    
-   
-//   return (
-//     <>
-//         <Box className='buxa' border={"1px solid gray"} borderRadius={10} padding={10}>
-//             <VStack spacing={4}>
-//                 <Image src='./public/ArtBongo2.png' style={{ width: '200px', height: 'auto' }} alt='ArtBongo Logo'/>
-                
-//                 {islogin ?<Login/> : <Signup/>}
-
-//                 <Flex alignItems={"center"} justifyContent={"center"} my={2} gap={1} w={"full"}>
-//                     <Box flex={2} h={"1px"} bg={"gray.400"}/>
-//                     <Text mx={1} color={"white"}>OR</Text>
-//                     <Box flex={2} h={"1px"} bg={"gray.400"}/>
-//                 </Flex>
-
-//                 <Googleauth />
-                
-//             </VStack>
-//         </Box>
-        // <Box className='buxa2' border={"1px solid gray"} borderRadius={10} padding={4}>
-        //     <Flex alignItems={"center"} justifyContent={"center"}>
-        //         <Box mx={1} fontSize={10} >
-        //             {islogin ? "Don't have an account?" : "Already have an account?"}
-        //         </Box>
-        //         <Box onClick={() => setlogin(!islogin)} color={"blue.500"} cursor={"pointer"} fontSize={10}>
-        //             {islogin ? "Sign Up" : "Log in"}
-        //         </Box>
-        //     </Flex>
-        // </Box>
-//     </>
-//   )
-// }
-
-// export default Authform
--- vite-project/src/components/Authform/Googleauth.jsx
+++ vite-project/src/components/Authform/Googleauth.jsx
@@ -1,13 +1,13 @@
 import { Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const Googleauth = () => {
+const Googleauth = ({prefix}) => {
     return (
         <>
             <Flex alignContent={"center"} justifyContent={"center"} cursor={"pointer"}>
                 <Image src='/google.png' w={4} alt='Google logo' />
                 <Text mx={1} color={"blue.300"} fontSize={10.5}>
-                    Log in with Google
+                    {prefix} with Google
                 </Text>
             </Flex>
         </>
